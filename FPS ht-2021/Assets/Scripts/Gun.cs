4b8dacabb163b6eb41c1e7cd2bf64b5aa8dc2222
Anders Brandt <75731366+Gunstringerlb@users.noreply.github.com>
2021-10-07 15:21:32 +0200

    Impact effekt, Automatic firerate och Sprint
    Nu funkar dessa funktioner!

---
+++ FPS ht-2021/Assets/Scripts/Gun.cs
@@ -0,0 +1,44 @@
+﻿
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public float firerate = 10f;
+
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+
+    private float nextTimeTofire = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeTofire)
+        {
+            nextTimeTofire = Time.time + 1f / firerate;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Enemy enemy = hit.transform.GetComponent<Enemy>();
+            if(enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f);
+        }
+    }
+}
