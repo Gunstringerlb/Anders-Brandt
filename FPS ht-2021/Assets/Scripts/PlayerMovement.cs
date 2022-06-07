d9a894a2c99700e4e6cb3060916066fd4a13649e
Anders Brandt <75731366+Gunstringerlb@users.noreply.github.com>
2022-06-07 16:23:31 +0200

    Ännu mer kommentaren =)

--- FPS ht-2021/Assets/Scripts/PlayerMovement.cs
+++ FPS ht-2021/Assets/Scripts/PlayerMovement.cs
@@ -93,8 +93,10 @@ public class PlayerMovement : MonoBehaviour
 
         Ray ray = new Ray(Camera.main.transform.position, transform.forward);
         RaycastHit hitInfo;
-        if(Input.GetAxis("Vertical") > 0  && Physics.Raycast(ray, out hitInfo, wallClimbDist))
+        // Om jag försöker gå framåt samt om det finns en vägg framför mig försök klättra.
+        if (Input.GetAxis("Vertical") > 0  && Physics.Raycast(ray, out hitInfo, wallClimbDist))
         {
+            // Om väggen lutar 90 +- "wallclimbangle" grader. Påbörja klättringen så fort spelaren tryckt ner mellanslag.
             if (hitInfo.normal.y <= Mathf.Sin(wallClimbAngle * Mathf.Deg2Rad) || hitInfo.normal.y >= -Mathf.Sin(wallClimbAngle * Mathf.Deg2Rad))
             {
                 if (Input.GetKeyDown(KeyCode.Space))
@@ -123,26 +125,30 @@ public class PlayerMovement : MonoBehaviour
         StartCoroutine(Climb(wallClimbTime, rayDirection));
     }
 
-    IEnumerator Climb(float time, Vector3 rayDirection) //
+    //Animation för att klättra på en vägg.
+    IEnumerator Climb(float time, Vector3 rayDirection) 
     {
         Vector3 startPos = transform.position;
         Vector3 endPos = transform.position + new Vector3(0, wallClimbHeight, 0);
 
-
+        //While loop som körs en gång varje frame i "time" sekunder.
         float timer = 0;
         while(timer < time)
         {
+            //Sätt velocity till 0 så att spelaren inte påverkas av gravitationen medans den klättrar. Därefter interpoleras positionen mellan startpositionen och slutpositionen för klättringen.
             velocity = Vector3.zero;
             transform.position = Vector3.Lerp(startPos, endPos, WallClimbSpeed(timer / time));
 
-
-            if((endPos - startPos) == new Vector3(0, wallClimbHeight, 0))
+            //Om vi fortfarande rör oss vertikalt så ska vi kolla efter en kant att klättra upp på. 
+            if ((endPos - startPos) == new Vector3(0, wallClimbHeight, 0))
             {
+                //Kolla efter en kant.
                 Ray ray = new Ray(Camera.main.transform.position, rayDirection + Vector3.down);
                 RaycastHit hitInfo;
                 if(Physics.Raycast(ray, out hitInfo, wallClimbDist * 2))
                 {
-                    if(hitInfo.normal.y >= Mathf.Sin((90-wallClimbAngle) * Mathf.Deg2Rad))
+                    //Om kanten inte lutar mer än "wallClimbAngle" grader så klättra upp på kanten genom att sätta ny startPos och endPos samt att återställa timern
+                    if (hitInfo.normal.y >= Mathf.Sin((90-wallClimbAngle) * Mathf.Deg2Rad))
                     {
                         startPos = transform.position;
                         endPos = hitInfo.point + Vector3.up * originalHeight / 2;
